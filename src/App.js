0bfdd491bed74a786c6f6dbdad0d155d603590c8
Ona Kotryna <ok.kuncyte@gmail.com>
2023-03-14 20:02:37 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
-  return <h1>The App</h1>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
